9291b60ab95be345007773f0db2668707abcaa5b
anthony62490 <anthonyjmagno@gmail.com>
2018-10-09 16:13:46 -0500

    set up routes and component structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { HashRouter } from 'react-router-dom'
+
 import Nav from './components/Nav';
-import Form from './components/Form';
-import Post from './components/Post';
-import Dashboard from './components/Dashboard';
-import Auth from './components/Auth';
+import routes from './routes';
+import './App.css';
 
 class App extends Component 
 {
   render() 
   {
     return (
-      <div className="App">
-        <Nav/>
-        <Form/>
-        <Post/>
-        <Dashboard/>
-        <Auth/>
-      </div>
+      <HashRouter>
+        <div className="App">
+          {routes}
+        </div>
+      </HashRouter>
     ); //return
   }
 }
---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import Auth from './components/Auth';
+import Dashboard from './components/Dashboard';
+import Post from './components/Post';
+import Form from './components/Form';
+
+
+export default (
+  <Switch>
+    <Route path='/' component={Auth}/>
+    <Route path='/dashboard' component={Dashboard}/>
+    <Route path='/post/:postid' component={Post}/>
+    <Route path='/new' component={Form}/>
+  </Switch>
+);
