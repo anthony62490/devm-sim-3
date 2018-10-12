91ef07e31bc7af6994f4dfc5312d2b32374693a4
anthony62490 <anthonyjmagno@gmail.com>
2018-10-12 01:07:30 -0500

    Added user post list to Dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,14 +1,41 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-class Dashboard extends Component {
-  render() {
+import { getPostsByUser } from '../redux/reducer';
+
+class Dashboard extends Component 
+{
+  componentDidMount()
+  {
+    if(this.props.id)
+    {
+      this.props.getPostsByUser(this.props.id);
+    }
+  }
+
+  render() 
+  {
+    let postMap;
+    if(this.props.userPosts)
+    {
+      postMap = this.props.userPosts.map((e,i) => 
+      {
+        return (
+          <Link to={`/post/${e.id}`}>
+            <h3>Go to Post #{e.id}</h3>
+          </Link>
+        );
+      });
+    }
+
     return (
       <div>
         Dashboard
         {this.props.username ? 
         <p>Logged in as: {this.props.username}</p>: 
         <p>Not logged in!</p>}
+        {postMap}
       </div>
     );
   }
@@ -16,4 +43,4 @@ class Dashboard extends Component {
 
 const mapStateToProps = (state) => state;
 
-export default connect(mapStateToProps, {})(Dashboard);
+export default connect(mapStateToProps, { getPostsByUser })(Dashboard);
